f31a69eaf04117a0da224076092324da61ecc2aa
Celine <CelineTSP@olds-imac.local>
2019-03-09 12:23:56 +0800

    Initial Commit

---
+++ Yanfong's notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yanfong's notes
+//
+//  Created by Celine on 9/3/19.
+//  Copyright © 2019 christopher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
